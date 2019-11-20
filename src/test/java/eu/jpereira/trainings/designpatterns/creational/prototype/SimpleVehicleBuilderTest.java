d9eb073a02887d9b58aab0a0cb4b48dc7986200a
Adam Mucha <adam.muszka@op.pl>
2019-11-20 19:18:58 +0100

    Prototype ver 2.0

---
+++ src/test/java/eu/jpereira/trainings/designpatterns/creational/prototype/SimpleVehicleBuilderTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2011 Joao Miguel Pereira
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.jpereira.trainings.designpatterns.creational.prototype;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import eu.jpereira.trainings.designpatterns.creational.prototype.exception.CouldNotCloneLastObjectException;
+import eu.jpereira.trainings.designpatterns.creational.prototype.exception.CannotHaveZeroPartsException;
+import eu.jpereira.trainings.designpatterns.creational.prototype.exception.NeedToPackLastVehicleException;
+import eu.jpereira.trainings.designpatterns.creational.prototype.exception.VehicleDoesNotHavePartsException;
+import eu.jpereira.trainings.designpatterns.creational.prototype.model.Shell;
+import eu.jpereira.trainings.designpatterns.creational.prototype.model.Tire;
+import eu.jpereira.trainings.designpatterns.creational.prototype.model.Vehicle;
+import eu.jpereira.trainings.designpatterns.creational.prototype.model.Window;
+
+/**
+ * 
+ * @author jpereira
+ * 
+ */
+public class SimpleVehicleBuilderTest {
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void cloneTest() throws CloneNotSupportedException, NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CouldNotCloneLastObjectException, CannotHaveZeroPartsException{
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		Vehicle vehicle = builder.createVehicle().packIt();
+		Vehicle hevTester = builder.createVehicle().with(new Tire()).times(5).packIt();
+		Vehicle _vehicle = (Vehicle) vehicle.clone();
+		
+		vehicle.setParts(hevTester.getAllParts());
+		
+		assertNotSame(_vehicle.countParts(),vehicle.countParts());
+		
+	}
+	
+	
+	@Test
+	public void testCreateVehicle() throws NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CloneNotSupportedException {
+
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		Vehicle vehicle = builder.createVehicle().packIt();
+		assertEquals(0, ((Vehicle) vehicle.clone()).countParts());
+
+	}
+	@Test
+	public void testTimesOne() throws NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CouldNotCloneLastObjectException, CannotHaveZeroPartsException, CloneNotSupportedException {
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		Vehicle vehicle = builder.createVehicle().with(new Tire()).times(1).packIt();
+		assertEquals(1, ((Vehicle) vehicle.clone()).countParts());
+		
+	}
+	@Test(expected=CannotHaveZeroPartsException.class)
+	public void testTimesZero() throws NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CouldNotCloneLastObjectException, CannotHaveZeroPartsException {
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		builder.createVehicle().with(new Tire()).times(0).packIt();
+	}
+	
+	@Test
+	public void testTimesMany() throws NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CouldNotCloneLastObjectException, CannotHaveZeroPartsException, CloneNotSupportedException {
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		Vehicle vehicle = builder.createVehicle().with(new Tire()).times(10).packIt();
+		assertEquals(10, ((Vehicle) vehicle.clone()).countParts());		
+	}
+	
+	@Test
+	public void testTimesManyDifferent() throws NeedToPackLastVehicleException, VehicleDoesNotHavePartsException, CouldNotCloneLastObjectException, CannotHaveZeroPartsException, CloneNotSupportedException {
+		SimpleVehicleBuilder builder = new SimpleVehicleBuilder();
+		Vehicle vehicle = builder.createVehicle().with(new Tire()).times(10).with(new Window()).times(2).with(new Shell()).packIt();
+		assertEquals(13, ((Vehicle) vehicle.clone()).countParts());		
+		
+	}
+
+
+
+}
